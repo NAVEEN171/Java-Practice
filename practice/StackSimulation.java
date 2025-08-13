0bd0ba0866821060e3ca74051c7e59bec570cb28
NAVEEN171 <naveenkumar171837@gmail.com>
2025-08-13 08:34:27 +0530

    updated enum

--- practice/StackSimulation.java
+++ practice/StackSimulation.java
@@ -1,73 +1,71 @@
 import java.util.ArrayList;
 import java.util.List;
-enum Status{
+
+enum Status {
     ACTIVE,
     INACTIVE
 }
 
-
-class StackUse<T>{
-     List<T> st;
+class StackUse<T> {
+    List<T> st;
     private int len;
-    private int idx=-1;
-    
-    
-   StackUse(int len){
-        if(len>0){
-           this.st=new ArrayList<>(len);
-           this.len=len;
-        }
-        else{
+    private int idx = -1;
+
+    StackUse(int len) {
+        if (len > 0) {
+            this.st = new ArrayList<>(len);
+            this.len = len;
+        } else {
             System.out.println("Enter Valid number");
         }
     }
-    public void push(T num){
-        if(idx+1<=len-1){
-            st.add(++idx,num);  
-        }
-        else{
+
+    public void push(T num) {
+        if (idx + 1 <= len - 1) {
+            st.add(++idx, num);
+        } else {
             System.out.println("Stack is filled! pop out some elements");
         }
-        
+
     }
-    public void pop(){
-        if(idx==-1){
+
+    public void pop() {
+        if (idx == -1) {
             System.out.println("We Cannot popup out elements! Stack is Empty");
-        }
-        else{
-          st.remove(idx--);
+        } else {
+            st.remove(idx--);
         }
     }
-    public void logStack(){
-        for(int i=0;i<=idx;i++){
-        
-          System.out.println("ele: "+st.get(i));
+
+    public void logStack() {
+        for (int i = 0; i <= idx; i++) {
+
+            System.out.println("ele: " + st.get(i));
         }
     }
 }
 
-class StackSimulation{
-    public static void main(String args[]){
-            StackUse<Integer> stackList=new StackUse<Integer>(5);
-            StackUse<String> stackStringList=new StackUse<String>(4);
-          
-    
-            System.out.println("String generics!");
-            stackStringList.push("apple");
-            stackStringList.push("Mango");
-            stackStringList.push(Status.ACTIVE.name());
-            stackStringList.logStack(); 
-            System.out.println();
-            
-            System.out.println("Integer generics!");
-            stackList.pop();
-            stackList.push(5);
-            stackList.push(4);
-            stackList.push(3);
-            stackList.push(5);
-            stackList.push(5);
-            stackList.push(5);
-            stackList.logStack(); 
-            System.out.println();          
+class StackSimulation {
+    public static void main(String args[]) {
+        StackUse<Integer> stackList = new StackUse<Integer>(5);
+        StackUse<String> stackStringList = new StackUse<String>(4);
+
+        System.out.println("String generics!");
+        stackStringList.push("apple");
+        stackStringList.push("Mango");
+        stackStringList.push(Status.ACTIVE.toString());
+        stackStringList.logStack();
+        System.out.println();
+
+        System.out.println("Integer generics!");
+        stackList.pop();
+        stackList.push(5);
+        stackList.push(4);
+        stackList.push(3);
+        stackList.push(5);
+        stackList.push(5);
+        stackList.push(5);
+        stackList.logStack();
+        System.out.println();
     }
 }
